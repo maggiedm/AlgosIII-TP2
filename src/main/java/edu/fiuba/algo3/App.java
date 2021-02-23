6c9a31d1c27c6a16383419ccd6b046befeaf9b41
Camilo Fábregas <50251894+MiloGNR@users.noreply.github.com>
2021-02-23 10:22:35 -0300

    App organizado
    Organicé el App para que quede un poco más limpio.

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -6,20 +6,15 @@ import edu.fiuba.algo3.javafx.layouts.LayoutAlgoritmo;
 import edu.fiuba.algo3.javafx.layouts.LayoutBloques;
 import edu.fiuba.algo3.javafx.layouts.LayoutDibujo;
 import edu.fiuba.algo3.javafx.layouts.LayoutEjecutarReiniciar;
-import edu.fiuba.algo3.modelo.Algoritmo;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import edu.fiuba.algo3.modelo.AlgoBlocks;
-
 
 /**
  * JavaFX App
@@ -32,16 +27,32 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         AlgoBlocks algoBlocks = new AlgoBlocks();
+        LayoutDibujo layoutDibujo = new LayoutDibujo(SCREEN_WIDTH/2,SCREEN_HEIGHT-(new HBox()).getHeight());
 
         // PANEL SUPERIOR: BANNER + BOTONES DE EJECUTAR Y REINICIAR
+        HBox panelSuperior = this.crearPanelSuperior(algoBlocks, layoutDibujo);
+
+        // PANEL INFERIOR: BLOQUES + ALGORITMO + DIBUJO
+        HBox panelInferior = this.crearPanelInferior(algoBlocks, layoutDibujo);
+
+        // PANEL PRINCIPAL: CONTIENE A panelInferior Y panelSuperior
+        VBox panelPrincipal = this.crearPanelPrincipal(panelSuperior, panelInferior);
+
+        // ESCENARIO
+        this.configurarEscenario(stage, panelPrincipal);
+        stage.show();
+    }
+
+    private HBox crearPanelSuperior(AlgoBlocks algoBlocks, LayoutDibujo layoutDibujo) {
         HBox panelSuperior = new HBox();
         ImageView banner = new ImageView("https://i.ibb.co/Ht7ysNy/New-Project-2.png");
-        LayoutDibujo layoutDibujo = new LayoutDibujo(SCREEN_WIDTH/2,SCREEN_HEIGHT-panelSuperior.getHeight());
         HBox layoutEjecutarReiniciar = LayoutEjecutarReiniciar.crear(algoBlocks, layoutDibujo);
         layoutEjecutarReiniciar.setAlignment(Pos.CENTER_LEFT);
         panelSuperior.getChildren().addAll(banner, layoutEjecutarReiniciar);
+        return panelSuperior;
+    }
 
-        // PANEL INFERIOR: BLOQUES + ALGORITMO + DIBUJO
+    private HBox crearPanelInferior(AlgoBlocks algoBlocks, LayoutDibujo layoutDibujo) {
         HBox panelInferior = new HBox();
         panelInferior.setSpacing(10);
         BloquesDisponibles bloquesDisponibles = new BloquesDisponibles();
@@ -52,26 +63,28 @@ public class App extends Application {
 
         VBox aux = new VBox(layoutAlgoritmo, botonGuardarAlgoritmo);
         panelInferior.getChildren().addAll(layoutBloques, aux, layoutDibujo);
+        return panelInferior;
+    }
 
-
-        // PANEL PRINCIPAL: CONTIENE A panelInferior Y panelSuperior
+    private VBox crearPanelPrincipal(HBox panelSuperior, HBox panelInferior) {
         VBox panelPrincipal = new VBox();
         panelPrincipal.setMaxSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         panelPrincipal.setSpacing(20);
         panelPrincipal.getChildren().addAll(panelSuperior, panelInferior);
+        return panelPrincipal;
+    }
 
-        // ESCENA
+    private void configurarEscenario(Stage stage, VBox panelPrincipal) {
         Scene scene = new Scene(panelPrincipal);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setHeight(SCREEN_HEIGHT);
         stage.setWidth(SCREEN_WIDTH);
-        stage.show();
     }
 
     public static void main(String[] args) {
         launch();
     }
-
+    
 }
 
